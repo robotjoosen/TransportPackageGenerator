48ec316b5eb560614aae3d9dd7d3512232f86c03
Roald Joosen <robotjoosen@gmail.com>
2019-09-10 09:01:14 +0200

    Check filepaths
    Set all file checks the same with error messages

--- app/build.transport.php
+++ app/build.transport.php
@@ -4,36 +4,54 @@ declare(strict_types=1);
 use RobotJoosen\TransportPackageGenerator\Builder;
 use Symfony\Component\Yaml\Yaml;
 
-require_once ROOT_PATH . 'vendor/autoload.php';
 define('ROOT_PATH', dirname(__FILE__) . DIRECTORY_SEPARATOR);
 define('MODX_BASE_PATH', getcwd() . DIRECTORY_SEPARATOR);
 define('MODX_CORE_PATH', MODX_BASE_PATH . 'core/');
 
+/** Check if dependencies are installed */
+$autoload_path = ROOT_PATH . 'vendor/autoload.php';
+if (file_exists($autoload_path)) {
+    require_once $autoload_path;
+} else {
+    echo "Looks like dependencies have not been installed with Composer.";
+    exit();
+}
+unset($autoload_path);
+
 /** Check if build configurator is present */
-if (file_exists(ROOT_PATH . 'build.config.php')) {
-    require_once ROOT_PATH . 'build.config.php';
+$build_config_path = ROOT_PATH . 'build.config.php';
+if (file_exists($build_config_path)) {
+    require_once $build_config_path;
 } else {
     echo "Build configuration file not found.\nRename build.config.sample.php to build.config.php and update values;\nOr use command below:\n~ mv build.config.sample.php build.config.php\n";
     exit();
 }
+unset($build_config_path);
 
 /** Check if MODX installation is present */
-if(
-    file_exists( MODX_CORE_PATH . 'config/config.inc.php') &&
-    file_exists(MODX_CORE_PATH . 'model/modx/modx.class.php')
+$modx_config_path = MODX_CORE_PATH . 'config/config.inc.php';
+$modx_class_path = MODX_CORE_PATH . 'model/modx/modx.class.php';
+if (
+    file_exists($modx_config_path) &&
+    file_exists($modx_class_path)
 ) {
-    require_once MODX_CORE_PATH . 'config/config.inc.php';
-    require_once MODX_CORE_PATH . 'model/modx/modx.class.php';
+    require_once $modx_config_path;
+    require_once $modx_class_path;
 
     /** Initialize Modx */
     $modx = new modX();
     $modx->initialize('mgr');
+} else {
+    echo "MODX was not found, check if you're in the right directory";
+    exit();
 }
+unset($modx_config_path, $modx_class_path);
 
 /** Get configuration settings */
-if (file_exists(PKG_PATH . 'package.config.yaml')) {
+$package_config_file = PKG_PATH . 'package.config.yaml';
+if (file_exists($package_config_file)) {
     try {
-        $config_yaml = file_get_contents(PKG_PATH . 'package.config.yaml');
+        $config_yaml = file_get_contents($package_config_file);
         $package_config = Yaml::parse($config_yaml);
     } catch (Symfony\Component\Yaml\Exception\ParseException $exception) {
         echo "Invalid package configuration file\n";
@@ -43,8 +61,10 @@ if (file_exists(PKG_PATH . 'package.config.yaml')) {
     echo "Configuration file not found: " . PKG_PATH . "package.config.yaml\n";
     exit();
 }
+unset($package_config_file);
 
 /** Start building */
+
 $builder = new Builder($modx, $package_config);
 $builder->build();
 unset($builder);
