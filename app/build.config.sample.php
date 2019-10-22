eeee6006d11ca44ba8e06759464391a8570ef998
Roald Joosen <robotjoosen@gmail.com>
2019-10-22 15:01:32 +0200

    Remove unused file

--- app/build.config.sample.php
+++
@@ -1,5 +0,0 @@
-<?php
-/**
- * Define the MODX path constants necessary for core installation
- */
- define('PKG_PATH', MODX_CORE_PATH . 'components/component_name/'); // TODO: Set path to your component
